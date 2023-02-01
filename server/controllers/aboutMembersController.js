7ffef81f829494fe803bd2d079b83d2fb45e118e
mikloska <miklosa973@gmail.com>
2023-01-31 21:04:01 -0500

    fixed error handling bugs.

--- server/controllers/aboutMembersController.js
+++ server/controllers/aboutMembersController.js
@@ -1,7 +1,6 @@
 const {executeQuery} = require("../config/executeQuery")
 
 const getAboutMembersController = async (req, res, next) => {
-  // try {
     const selectQuery = 
       ` SELECT m.id, m.first_name, m.last_name, ei.instrument_name AS english_instruments, hi.instrument_name AS hungarian_instruments, eb.bio AS english_bio, hb.bio AS hungarian_bio
         FROM musicians AS m
@@ -15,16 +14,7 @@ const getAboutMembersController = async (req, res, next) => {
         ON m.id = hb.member_id
         ORDER BY rank;
       `
-    const queryResult = await executeQuery(selectQuery, next)
-    if(queryResult){
-      res.json(queryResult.rows)
-    } else {
-      return next(new Error(`Error in about members controller: ${error.message}`))
-    }
-    
-  // } catch(error) {
-  //   return next(new Error(`Error in about members controller: ${error.message}`))
-  // }
+    await executeQuery(selectQuery, next)
 }
 
 const updateAboutMemberController = async (req, res, next) => {
