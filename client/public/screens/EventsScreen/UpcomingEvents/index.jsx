a215814d9d245d89e9214fd5dcacfdb92ca910dc
mikloska <miklosa973@gmail.com>
2023-02-03 10:11:05 -0500

    Removed unused code and console.log statements from event screens.

--- client/public/screens/EventsScreen/UpcomingEvents/index.jsx
+++ client/public/screens/EventsScreen/UpcomingEvents/index.jsx
@@ -36,6 +36,7 @@ const UpcomingEvents = ({ events }) => {
 
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [rows, setRows] = useState([])
   const columnsTranslation = {
     'Event' : 'Esemény',
     'Venue' : 'Helyszín',
@@ -55,31 +56,10 @@ const UpcomingEvents = ({ events }) => {
     ] 
   );
 
-  const [rows, setRows] = useState([
-    // {name: 'Christmas Concert', 'venue': 'Magyar Tanya', 'address': '1495 Huffs Church Rd, Barto, PA 19504', 'dateAndTime': 'December 4, 2022 2:00 PM', 'flyer': <IconButton color='primary'><ImageIcon/></IconButton>},
-    // {name: 'Dance House', 'venue': 'Hungarian House', 'address': '213 E 82nd St, New York, NY 10028', 'dateAndTime': 'December 3, 2022 7:00 PM', 'flyer': <IconButton disabled={true} color='primary'><ImageIcon/></IconButton>},
-    // {name: 'Mikulás (Santa Claus)', 'venue': 'Reka Darida Foundation', 'address': '1065 Madison Ave, New York, NY 10028', 'dateAndTime': 'December 3, 2022', 'flyer': <IconButton color='primary'><ImageIcon/></IconButton>},
-    // {name: 'Hungarian Bazaar', 'venue': 'Women\'s Club of Chevy Chase', 'address': '7931 Connecticut Ave, Chevy Chase, MD 20815', 'dateAndTime': 'December 19, 2022 1:00 PM', 'flyer': <IconButton color='primary'><ImageIcon/></IconButton>},
-  ])
-
   useEffect(() => {
-    console.log(events)
-    const temp = []
     if(events.length > 0){
       setRows(events)
-    // events.forEach((event) => {
-    //   setRows([
-    //     ...rows,
-    //     { name: event.event_name, venue: event.venue, address: event.address, dateAndTime: event.date_and_time, 
-    //       'flyer': <IconButton color='primary' disabled={event.flyer.length > 1 ? false : true} target="_blank" href={event.flyer}><ImageIcon/></IconButton>,
-    //       'schedule': <IconButton color='primary' disabled={event.schedule.length > 1 ? false : true} target="_blank" href={event.schedule}><ImageIcon/></IconButton>
-    //     }
-    //   ])
-    // }) 
-    // setRows(temp)
-  }
-  
-
+    }
   }, [events])
 
   useEffect(() => {
