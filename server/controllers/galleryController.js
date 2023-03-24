e515ab659ac36a6b1880bc3b9cb04acad01311c0
mikloska <miklosa973@gmail.com>
2023-03-24 13:54:17 -0400

    Added npm verion to package.json

--- server/controllers/galleryController.js
+++ server/controllers/galleryController.js
@@ -1,5 +1,4 @@
 const {executeQuery} = require("../config/executeQuery")
-const { getFileStream } = require("../s3")
 
 const galleryController = async (req, res, next) => {
   const selectQuery = "SELECT * FROM gallery;"
