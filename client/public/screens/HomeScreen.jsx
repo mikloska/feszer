0fd6ac72c91ee340f67c206f8b9dee9b3093932a
mikloska <miklosa973@gmail.com>
2023-06-12 21:50:28 -0400

    fixed youtube video bug.

--- client/public/screens/HomeScreen.jsx
+++ client/public/screens/HomeScreen.jsx
@@ -6,33 +6,27 @@ import { Edit as EditIcon } from '@mui/icons-material';
 
 
 import { changeLoading } from '../../redux/slices/loadingSlice';
-import { useGetVideosQuery, useUpdateVideoMutation } from '../../redux/slices/videosSlice';
 import { useGetAboutBandQuery, useUpdateAboutBandMutation } from '../../redux/slices/aboutBandSlice';
 import { ErrorModal } from '../components/ErrorModal';
 import { EditModal } from '../components/EditModal/EditModal';
-import { EditVideosModal } from '../components/EditVideosModal/EditVideosModal';
 
 const HomeScreen = () =>{
   const dispatch = useDispatch()
   const loggedIn = useSelector((state) => state.loggedIn.value) 
   const language = useSelector((state) => state.language.value)
   const { data, error, isLoading, refetch } = useGetAboutBandQuery()
-  const { videos, videoFetchError, videosLoading, refetchVideos } = useGetVideosQuery()
   const [edit, setEdit] = useState(false)
-  const [editVideos, setEditVideos] = useState(false)
   const [newData, setNewData] = useState('')
-  const [newVideo, setNewVideo] = useState('')
   const [updateAboutBand, { updateLoading }] = useUpdateAboutBandMutation();
-  const [updateVideo, { updateVideoLoading }] = useUpdateVideoMutation();
 
   useEffect(() => {
     // This will be used for the query
-    if(isLoading || updateLoading || videosLoading || updateVideoLoading){
+    if(isLoading || updateLoading){
       dispatch(changeLoading({"loading":true}))
     } else {
       dispatch(changeLoading({"loading":false}))
     }
-  }, [isLoading, updateLoading, videosLoading, updateVideoLoading])
+  }, [isLoading, updateLoading])
   
 
   return (
@@ -54,34 +48,14 @@ const HomeScreen = () =>{
           usage={'band'}
         />
       }
-      
-      {loggedIn && videos && editVideos && 
-        <EditVideosModal 
-          videos={videos}
-          setEditVideos={setEditVideos}
-          setNewVideo={setNewVideo}
-          newVideo={newVideo}
-          updateVideo={updateVideo}
-          refetchVideos={refetchVideos}
-        />
-      }
       {/* {error && 
         <ErrorModal error={error.data.message}/>
       } */}
-      {videos && 
-        <Grid item md={5} sm={12} style={{textAlign:'left'}}>
-          {loggedIn &&
-            <Button variant="contained" color="primary" style={{marginTop:20}} onClick={() => setEditVideos(true)}>
-              Modify Video urls
-            </Button>
-          }
-          {
-            videos.map(video => (
-              <iframe width='100%' height='225' src={video.url} key={`${video.url}-${video.id}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-            ))
-          }
-        </Grid>
-      }
+      <Grid item md={5} sm={12} style={{textAlign:'left'}}>
+        <iframe width='100%' height='225' src="https://www.youtube.com/embed/dgqVcvD2hc4" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        <iframe width='100%' height='225' src="https://www.youtube.com/embed/VLbdGoYf3_k" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        <iframe width='100%' height='225' src="https://www.youtube.com/embed/OQacr-i-V28" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      </Grid>
       <Grid item  md={5} sm={12} style={{textAlign:'left'}}>
         {data && 
           <Typography>
@@ -89,12 +63,11 @@ const HomeScreen = () =>{
               language==='MAGYAR'?
               data.english : data.magyar
             }
-            {
-              loggedIn &&
-                <IconButton onClick={() => setEdit(true)}>
-                  <EditIcon color='primary'/>
-                </IconButton>
-            }
+                  {loggedIn &&
+        <IconButton onClick={() => setEdit(true)}>
+          <EditIcon color='primary'/>
+        </IconButton>
+      }
           </Typography>
         }
         <Grid item  md={4} sm={12} style={{textAlign:'center'}}>
